3d345c847e0a6dcf6de1f54d068d2ed59ead7809
Fernando Iazeolla <fernando.iazeolla@gmail.com>
2018-05-13 12:24:02 +0200

    added json encoding option and encodeURI option. code refactored. changed dit to web iso test.

--- web/serialize.js
+++ web/serialize.js
@@ -10,7 +10,7 @@ class Serialize{
     			}
 				}
 			}
-			//console.log(object);
+			console.log(object);
 			return object;
 		}
 		for (var k in object){
@@ -20,6 +20,7 @@ class Serialize{
     	}
 		}
 		return uriencode ? s.join('&').replace(/%20/g, '+') : s.join('&');
+		//return object;
 	}
 
 	serialize(element, encodetype = 'form', uriencode = null) {
@@ -49,10 +50,52 @@ class Serialize{
 				}
 			}
 		}
+		//console.log(s,obj);
 		return this.encode(obj, encodetype, uriencode);
+		//return s.join('&').replace(/%20/g, '+');
 	}
 
+	serialize2(form) {
+		var field, s = [];
+		if (typeof form == 'object' && form.nodeName == "FORM") {
+			var len = form.elements.length;
+			for (var i=0; i<len; i++) {
+				field = form.elements[i];
+				if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+					if (field.type == 'select-multiple') {
+						for (var j=form.elements[i].options.length-1; j>=0; j--) {
+							if(field.options[j].selected)
+								s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
+						}
+					} else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+						s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
+					}
+				}
+			}
+		}
+		return s.join('&').replace(/%20/g, '+');
+	}
+	
+	serialize_div(form) {
+		var field, s = [];
+		if (typeof form == 'object' && form.nodeName == "DIV") {
+			var len = form.elements.length;
+			for (var i=0; i<len; i++) {
+				field = form.elements[i];
+				if (field.name && !field.disabled && field.type != 'file' && field.type != 'reset' && field.type != 'submit' && field.type != 'button') {
+					if (field.type == 'select-multiple') {
+						for (var j=form.elements[i].options.length-1; j>=0; j--) {
+							if(field.options[j].selected)
+								s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.options[j].value);
+						}
+					} else if ((field.type != 'checkbox' && field.type != 'radio') || field.checked) {
+						s[s.length] = encodeURIComponent(field.name) + "=" + encodeURIComponent(field.value);
+					}
+				}
+			}
+		}
+		return s.join('&').replace(/%20/g, '+');
+	}
 }
 
-module.exports = new Serialize();
 
